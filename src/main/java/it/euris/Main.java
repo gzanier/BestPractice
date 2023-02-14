4c11be87fe121b51ef188df36c899b1950ec667a
gzanier <46709701+gzanier@users.noreply.github.com>
2023-02-14 10:38:21 +0100

    convertito il Main con java-8 HashSet

--- src/main/java/it/euris/Main.java
+++ src/main/java/it/euris/Main.java
@@ -4,15 +4,24 @@ import it.euris.pojo.Circle;
 import it.euris.pojo.Square;
 import it.euris.service.AreaCalculator;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Collection<Object> shapes = Set.of(
+
+        Collection<Object> shapes = new HashSet<>();
+        //new ArrayList<>();
+        shapes.add(new Circle(2));
+        shapes.add(new Square(5));
+        shapes.add(new Square(6));
+
+                /*Set.of(
             new Circle(2),
             new Square(5),
             new Square(6)
-        );
+        );*/
 
         AreaCalculator areaCalculator = new AreaCalculator(shapes);
 
